c5188695e3f9521146d9ce66778d326488c5201d
Jonathan Coombs <jvcoombs@gmail.com>
2015-01-08 15:54:30 -0600

    Auto-remove front sides found on back sides. But introduced a regression: max recursion

--- flashgrid/reviewerMonkey.py
+++ flashgrid/reviewerMonkey.py
@@ -1,5 +1,5 @@
-''' This is a monkey patch on reviewer.py
-It adds a grid dialog in a popup window
+''' This is an addon (/extension/plugin) to the Reviewer class found in reviewer.py
+It adds a grid dialog (currently as a popup window)
 '''
 from aqt.reviewer import Reviewer
 from aqt import mw
@@ -11,14 +11,17 @@ from aqt.webview import AnkiWebView
 #from grid import Ui_gridDialog
 from main import GridDlg
 
-def myShowQuestion(self):
-    origShowQuestion(self) # we're wrapping this, so we'll still run it
+def onShowQuestion():
+    rev = mw.reviewer
+    
+    #origShowQuestion(rev) # we're wrapping this, so we'll still run it
+    
     if not GridDlg.gridOn: return
     
-    #showInfo(self.state)
+    #showInfo(rev.state)
     #return
 
-    if not self.state == "question":  # did the user just click to get a new question?
+    if not rev.state == "question":  # did the user just click to get a new question?
         # Yes  
         return
     # Probably not. # TODO: find something that blocks this when in "question" mode (e.g. on closing the Fields dialog) 
@@ -27,18 +30,18 @@ def myShowQuestion(self):
     #time.sleep(2)
 
     #showInfo("pretend this is a grid")
-    w = GridDlg(self, Reviewer.gridSize())
-    w.resize(1024, 600)  # dialog window size: W, H
-    w.move(100,30) # TODO: automate this based on screen size, but remember (this session) if the user moves/resizes it. Simplest option is to not to destroy the object on close. 
+    w = GridDlg(rev)
+    w.resize(800, 530)  # dialog window size: W, H
+    w.move(224,150) # TODO: automate this based on screen size, but remember (this session) if the user moves/resizes it. Simplest option is to not to destroy the object on close. 
     
     v = w.ui.gridView  # type of v: QtWebKit.QWebView or its AnkiWebView subclass
 
     #see also: Reviewer._initWeb()
-    base = getBase(self.mw.col)  # this is necessary, or the images won't display  
+    base = getBase(rev.mw.col)  # this is necessary, or the images won't display  
 
-    html = GridDlg.gridHtml(self._css, base) #... we insert this once for the whole grid
+    html = GridDlg.gridHtml(rev._css, base) #... we insert this once for the whole grid
 
-    callback = lambda x: self._showAnswerGrid(self.card, w)
+    callback = lambda x: w.showAnswerGrid(rev.card, rev)
 
     v.setHtml(html, callback)  # pass in the 'empty' container, plus the function that'll fill it in
 
@@ -49,147 +52,30 @@ def myShowQuestion(self):
 
     if w.exec_():
         # if it returns 1, show the next flashcard
-        self._showAnswer() # WARNING: this could eventually cause a stack overflow, I think, but there's currently no hook for asking Anki to do this for us. -Jon  
+        rev._showAnswer() # WARNING: this could eventually cause a stack overflow, I think, but there's currently no hook for asking Anki to do this for us. -Jon  
 
 
-def myShowAnswerGrid(self, card, dialog):
-    
-    view = dialog.ui.gridView
-    
-    #view.setHtml( self.web.page().mainFrame().toHtml() )  # DELETE THIS LINE (just a test)
-    #return
-    
-    gridSize = Reviewer.gridSize()
 
-    deckName = mw.col.decks.current()['name']
-    cards = mw.col.findCards("-is:suspended deck:%s" % deckName)
-    #e.g.  mw.col.findCards('-is:suspended deck:indonesian-lift-dictionary-Orig')
+from anki.hooks import addHook
+addHook('showQuestion', onShowQuestion)
 
-    toInsert = ''
-    i = 1
-    for c in cards:  # at most; but usually we'll quit at gridSize^2
-        if (c == card.id):
-            continue
-        if (i == dialog.correct):
-            cellCard = card
-        else:
-            cellCard = mw.col.getCard(c) # mw.col.findCards("cid:%s" % c)
-            if not cellCard or (cellCard.template() != card.template()):
-                continue  # something went wrong finding that card (throw exception?)
 
-        html = renderOneAnswer(self, cellCard)
-        toInsert += GridDlg.gridHtmlCell(i, html)  # this += isn't likely to ever get slow, but if so, build an ins[] list and later do ''.join(ins)
-        if ((i % gridSize == 0) and (i < gridSize*gridSize)):  # use modulus to identify/create end of row
-            toInsert += GridDlg.gridHtmlBetweenRows()
 
-        if (i >= gridSize*gridSize):
-            break
-        i += 1
+# This UGLY MONKEY PATCh is because the actual code is in the wrong order:
 
-    klass = "card card%d" % (card.ord+1)
-    toInsert = '<table class="%s"><tbody><tr>%s</tr></tbody></table>' % (klass, toInsert) 
-    #test: toInsert += '<img src="file:///c:/Users/user57/Documents/Anki/User 1/collection.media/lift-dictionary_abu.png" />'
-    tmp = json.dumps(toInsert)
-    tmp = '_append("%s", %s);' % ('insertGridHere', tmp)
-
-    #tmp = '_updateA("%s", %s);' % (str(cellId), html)
-    #self.web.eval(tmp)  # NO, instead of the Reviewer doing this, have the view do it
-    view.page().mainFrame().evaluateJavaScript(tmp)
-
-    cardFrontHtml = renderOneAnswer(self, card)
-    tmp = json.dumps(cardFrontHtml)
-    tmp = '_append("%s", %s);' % ('insertFrontHere', tmp)
-    view.page().mainFrame().evaluateJavaScript(tmp)
-
-    #self._showEaseButtons() # NO, not in the grid
-   
-    h = view.page().mainFrame().toHtml()
-    f = open('gridtemp.tmp.html', 'w')
-    f.write(h)
-    f.close()
-   
-    '''
-    callback = renderOneAnswer(self, card, view, "1")
-    actualCard = AnkiWebView()  # a view we won't actually display, but it renders (off-screen) one cell's HTML for us
-    actualCard.stdHtml(self._revHtml, self._styles(),
-        loadCB=callback,
+def myInitWeb(self):
+    self._reps = 0
+    self._bottomReady = False
+    base = getBase(self.mw.col)
+    # show answer / ease buttons
+    self.bottom.web.show()
+    self.bottom.web.stdHtml(
+        self._bottomHTML(),
+        self.bottom._css + self._bottomCSS,
+        loadCB=lambda x: self._showAnswerButton())
+    # main window
+    self.web.stdHtml(self._revHtml, self._styles(),
+        loadCB=lambda x: self._showQuestion(),
         head=base)
 
-    callback = renderOneAnswer(self, card, view, 4)
-    actualCard = AnkiWebView()  # a view we won't actually display, but it renders one cell's HTML for us
-    actualCard.stdHtml(self._revHtml, self._styles(),
-        loadCB=callback,
-        head=base)
-    '''
-    
-    
-    #frames = v.page().mainFrame().childFrames()
-    #for frame in frames:
-    #    frame.setHtml(h1)
-    #frames[0].setHtml(h2)
-    #v.setHtml(h2)
-    
-    '''
-    # need a loop here
-    flakCard = AnkiWebView()  # again, a view we won't actually display, but it renders a cell's HTML for us
-    flakCard.stdHtml(self._revHtml, self._styles(),
-        loadCB=callback,
-        head=base)
-    '''
-
-
-def renderOneAnswer(self, card):
-    ''' Creates HTML to plug directly in as the specified <TD> table cell.
-    '''
-
-    origState = self.state
-    self.state = "answer"  # necessary to get correct results from _mungeQA()
-    
-    c = card
-    
-    #a = c._getQA()['a'] # without the style info
-    a = c.a() # with the style info
-    
-    # play audio?  # NO, not in the grid (except maybe on hover?)
-    #if self.autoplay(c):
-    #    playFromText(a)
-    # render and update bottom
-    
-    a = self._mungeQA(a)
-    klass = "card card%d" % (c.ord+1)
-    #tmp = "_updateA(%s, true, %s);" % (json.dumps(a), klass)
-
-    # DON'T show answer / ease buttons
-
-    self.state = origState
-
-    html = a
-    # html = json.dumps(a) # NOT until we've added our html
-
-    # user hook
-    #runHook('showAnswer')  # NO, we assume other addons' behavior here is unwanted
-
-    div = '<div class="%s">%s</div>'
-    tmp = div % (klass, html)
-    return tmp
-        
-@staticmethod
-def myGridSize():
-    return Reviewer._gridSize
-
-@staticmethod
-def myToggleGridSize():
-    Reviewer._gridSize = 3 if (Reviewer._gridSize == 2) else 2
-    showInfo("Ok. Toggled to %s x %s." % (Reviewer._gridSize, Reviewer._gridSize))  # msgbox / messagebox
-
-# Do some monkey patching to wrap more functionality around _showQuestion    
-
-origShowQuestion = Reviewer._showQuestion
-Reviewer._showQuestion = myShowQuestion
-
-Reviewer._showAnswerGrid = myShowAnswerGrid
-
-# TODO: consider moving gridSize() over to main.py where it would be more legit (i.e. no monkey patch)
-Reviewer._gridSize = 2
-Reviewer.toggleGridSize = myToggleGridSize
-Reviewer.gridSize = myGridSize
+Reviewer._initWeb = myInitWeb
